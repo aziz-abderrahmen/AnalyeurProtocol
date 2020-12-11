b3dffa7a5afb8a693fda6d701d35076021c7583b
Ohenheim <pepsi.ibrahim@gmail.com>
2020-12-11 18:09:59 +0100

    Test

---
+++ Protocols/Protocol.java
@@ -0,0 +1,23 @@
+package Protocols;
+
+import java.util.List;
+
+public abstract class Protocol {
+	protected final int headerLength; // In bytes
+    protected final int footerLength; // In bytes
+    private final List<String> payload; // charge utile
+    protected final List<String> data;
+
+    public Protocol(List<String> data, int headerLength, int footerLength) {
+        this.data = data;
+        this.headerLength = headerLength;
+        this.footerLength = footerLength;
+        this.payload = data.subList(headerLength, data.size() - footerLength);
+    }
+
+    public List<String> getPayload() {
+        return payload;
+    }
+}
+
+
